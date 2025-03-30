d4b6192f859ee99a578045dae8c5820ec51ceb75
Niktin Egor <nda60@ya.ru>
2025-03-30 22:34:40 +0300

    Added some basic tests

--- src/test/kotlin/trees/BinarySearchTests.kt
+++ src/test/kotlin/trees/BinarySearchTests.kt
@@ -1,10 +1,9 @@
 package trees
 
-import BST.BinarySearchTree
+import bst.BinarySearchTree
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.BeforeEach
 
 class BinarySearchTests {
 
@@ -13,11 +12,20 @@ class BinarySearchTests {
 
         @Test
         fun `insertion of one element`() {
-            val tree =  BinarySearchTree<Int, String>()
-            tree.insert(10)
+            val tree = BinarySearchTree<Int, String>()
+            tree.insert(10, "10")
             assertTrue(tree.contains(10))
-
         }
+
+        @Test
+        fun `insertion of multiple elements`() {
+            val tree = BinarySearchTree<Int, String>()
+            tree.insert(35, "35")
+            tree.insert(100, "100")
+            tree.insert(0, "0")
+            assertTrue(tree.contains(35))
+            assertTrue(tree.contains(100))
+            assertTrue(tree.contains(0))
     }
 
 
