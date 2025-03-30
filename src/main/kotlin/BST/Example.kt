ef43df19f7989d9ff55fd4870e0d4be8b0e401c9
Egor Nikitin <51236949+End0ra@users.noreply.github.com>
2025-03-30 20:17:54 +0300

    Update Example.kt
    Fixed the commit. Added an example of using BST.

--- src/main/kotlin/BST/Example.kt
+++ src/main/kotlin/BST/Example.kt
@@ -1,3 +1,22 @@
 package BST
 
-fun main
+fun main() {
+    val tree = BinarySearchTree<Int, String>()
+
+    tree.insert(4, "D")
+    tree.insert(2, "B")
+    tree.insert(6, "F")
+    tree.insert(1, "A")
+    tree.insert(3, "C")
+    tree.insert(5, "E")
+    tree.insert(7, "G")
+    println(tree.iteration()) // Выведет [(4, D), (2, B), (6, F), (1, A), (3, C), (5, E), (7, G)]
+
+    tree.delete(1)
+    println(tree.iteration()) // Выведет [(4, D), (2, B), (6, F), (3, C), (5, E), (7, G)]
+
+    tree.insert(1, "Hmm")
+    println(tree.iteration()) // Выведет [(4, D), (2, B), (6, F), (1, Hmm), (3, C), (5, E), (7, G)]
+
+
+}
