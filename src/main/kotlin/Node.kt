7f95e58c7168fc1b73ad531e7898f9c91906d626
Niktin Egor <nda60@ya.ru>
2025-03-31 20:41:05 +0300

    attempt to create new feedback branch

--- src/main/kotlin/Node.kt
+++
@@ -1,4 +0,0 @@
-class Node <K : Comparable<K>, V>(var key: K, var value: V) {
-    var left: Node<K, V>? = null
-    var right: Node<K, V>? = null
-}
--- src/main/kotlin/Tree.kt
+++
@@ -1,7 +0,0 @@
-interface Tree<K : Comparable<K>, V> {
-    fun insert(key: K, value: V)
-    fun search(key: K): V?
-    fun delete(key: K)
-    fun iteration(): List<Pair<K, V>>
-}
-
