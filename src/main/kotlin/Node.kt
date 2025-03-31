11c29930eaf80a1d0f366493afd9bb93a02c3a31
desiredeternity <don.bdf2011@mail.ru>
2025-03-31 17:13:37 +0300

    make Node class open

--- src/main/kotlin/Node.kt
+++ src/main/kotlin/Node.kt
@@ -1,4 +1,4 @@
-class Node <K : Comparable<K>, V>(var key: K, var value: V) {
+open class Node <K : Comparable<K>, V>(var key: K, var value: V) {
     var left: Node<K, V>? = null
     var right: Node<K, V>? = null
 }
