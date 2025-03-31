8858ece902ca15c75b382f67d308dd12b2bbfcc8
Vladimir <khmelevvladimir2006@gmail.com>
2025-03-31 23:41:45 +0300

    Delete src/main/kotlin/AVL directory

--- src/main/kotlin/AVL/AVLNode.kt
+++
@@ -1,9 +0,0 @@
-package AVL
-
-class AVLNode<K : Comparable<K>, V>(
-    var key: K,
-    var value: V,
-    var left: AVLNode<K, V>? = null,
-    var right: AVLNode<K, V>? = null,
-    var height: Int = 1
-)
