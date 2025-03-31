850a6b4c438867ccb26524f7528f36650335cf09
Vladimir Khmelev <khmelevvladimir2006@gmail.com>
2025-03-31 23:15:40 +0300

    added AVLNode, AVLTree, AVLTest. The main functions are implemented: insert, delete, iterator, search. Added tests - unit and property

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,23 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+
 }
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+        showStandardStreams = true
+    }
 }
 kotlin {
     jvmToolchain(23)
