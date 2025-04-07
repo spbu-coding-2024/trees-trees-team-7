a3835ef51883cf2bf75b6eadb0be5db62ec89fc0
desiredeternity <don.bdf2011@mail.ru>
2025-04-07 18:09:59 +0300

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.1.10"
+    jacoco
 }
+
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
@@ -11,13 +13,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
     testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
 
 tasks.test {
@@ -26,7 +27,18 @@ tasks.test {
         events("passed", "skipped", "failed")
         showStandardStreams = true
     }
+    ignoreFailures = true
+    finalizedBy(tasks.jacocoTestReport)
 }
 kotlin {
-    jvmToolchain(23)
+    jvmToolchain(22)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
+        csv.required.set(false)
+    }
 }
