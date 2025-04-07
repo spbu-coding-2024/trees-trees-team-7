30282714c5a09a65b352ccde1a7d2079ab0d0018
Daniil Ivanov <don.bdf2011@mail.ru>
2025-04-07 23:00:29 +0300

    code format using ktlint (#17)
    * code format using ktlint
    
    * delete old appeared BinarySearchPropertyTest.kt file due to renaming

--- src/main/kotlin/trees/avl/AVLNode.kt
+++ src/main/kotlin/trees/avl/AVLNode.kt
@@ -3,10 +3,10 @@ package trees.avl
 import Node
 
 class AVLNode<K : Comparable<K>, V>(
-    key: K, 
-    value: V
+    key: K,
+    value: V,
 ) : Node<K, V, AVLNode<K, V>>(key, value) {
-    var height: Int = 1 
+    var height: Int = 1
     val balanceFactor: Int
         get() = (left?.height ?: 0) - (right?.height ?: 0)
 }
